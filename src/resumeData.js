e5bb86c91dd4865c30a4508e6eacfde5722dc150
Patrick Follett <pwf592110@gmail.com>
2019-09-30 13:59:30 -0400

    updating

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,59 +1,59 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
+    "imagebaseurl":"",
+    "name": "Patrick Follett",
     "role": "Frontend Developer and Data Scientist",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": "I like working in various parts of frontend development and backend development and like to learn about new technologies whenever I have free time or simply play video games. I played volleyball all my life and still do to this day",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/patrickwfollett/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/PWFollett",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "url":"http://twitter.com/LaPat_Blue",
           "className":"fa fa-twitter"
         }
       ],
     "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
     "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "website":"https://github.com/PWFollett",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"University of Pennsylvania LPS Coding BootCamp",
+        "specialization":"Full Stack Development",
+        "MonthOfPassing":"October",
+        "YearOfPassing":"2019",
+        "Achievements":"Graduated"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Ohio University",
+        "specialization":"Political Marketing and Strategic Communications",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        "Achievements":"Graduated 3.5/4.0"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"FedEx Services ",
+        "specialization":"Senior Account Exective",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2019",
+        "Achievements":"Ambassadors Club 2018"
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
+        "CompanyName":"PLS Logistics",
+        "specialization":"Third Party Logisitics Broker",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2016",
         "Achievements":"Some Achievements"
       }
     ],
@@ -93,8 +93,8 @@ let resumeData = {
     ],
     "testimonials":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":" worked with Patrick on several projects throughout our time studying online with the University of Pennsylvania's LPS Coding Bootcamp. Saying Patrick is a team player and a really great person to work with would be quite a heavy understatement. Patrick Follett has a clear drive to learn and hone his craft as a professional developer, he does everything he can to reach his goals and is always willing to stay up some nights to help reach MVP on projects during tedious time crunches with little time left to get the job done. He's a quick learner with a passion for coding, but also, he's a great guy with an awesome personality! I would highly, highly recommend Mr. Follett to any company. He's not just an asset to your team, but a wonderful co-worker and friend and is always willing to help with complex issues whenever you need it.",
+        "name":"Shawn Luther, Jr. Web Developer" 
       },
       {
         "description":"This is a sample testimonial",
