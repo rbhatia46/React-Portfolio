ea9fb622073d0f2f0390183f27c540aef05ff9a3
StacyWilliams <stacymsiverd@gmail.com>
2019-10-28 09:58:51 -0500

    changed back

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,7 +15,7 @@ export default class Porfolio extends Component {
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
                     <a href="#modal-01">
-                    <div style="width:100%;height:0;padding-bottom:67%;position:relative;"><iframe src="https://giphy.com/embed/JPObaH0nzNohW" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/water-drop-gif-JPObaH0nzNohW">via GIPHY</a></p>
+                      <img src={item.imgurl} className="item-img" />
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
