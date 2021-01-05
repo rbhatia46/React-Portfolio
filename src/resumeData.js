0e115e563a4658b18bb3165236ed0798d4c0719d
Tyler Bolty <tsbolty@gmail.com>
2021-01-04 21:28:55 -0700

    added github pages deployment configuration

--- src/resumeData.js
+++ src/resumeData.js
@@ -60,13 +60,13 @@ let resumeData = {
     "skillsDescription":"Top Skills",
     "skills":[
       {
-        "skillname":"React.js, HTML5, CSS3, JavaScript, JQuery, Bootstrap, Sass, Handlebars, UX"
+        "skillname":"React.js"
       },
       {
-        "skillname":"Node, Express.js, MongoDB, Mongoose, MySQL, Sequelize, Postgres, User Authentication, "
+        "skillname":"Node"
       },
       {
-        "skillname":"Search-Engine-Optimization, Database Architecture, RESTful API's, Object-Oriented-Programming, Progressive Web Apps, Agile Development"
+        "skillname":"Agile Development"
       }
     ],
     "portfolio":[
@@ -76,9 +76,9 @@ let resumeData = {
         "imgurl":"https://lh3.googleusercontent.com/ilitkk9VbLNVm6nbfnYaihJ1mcL3RIfXs0R96Ueg2IyPPgE8r0p4LEqxdnm3AqF8Osz7sQ2sQNn6QRWYb8G3QZx44WwlEH9lInY5ZV7hdr0Jl0o42ZDq18mMU4aHuHxhFfIuYn6aKldk97cVEWi1ZpOR2thWmhLgCrPUjQHlB-v2HyU7-m371EPkIudwTDyysiP0PdoksmXHZLD5puVXSZ7dcAtm3Hmf_xWCpesJEU902JwsG7QnCzdp8fE17YgWEklZdVFKrkeBcFbCbx-wznUeMiuBfsFBtDtDi-XDDlIbLF4FDGA7NpFguhUhiJ-Xa5ybbz_v0qu5P9ZjCYkLDPvekhGrjwPYYevpQLA9TkqaeDLzdTiIn9CbhxSFsCPx6uHODpS5HdToMsSdOFIZZnpJHSM9MPDQY_9gdFVCiVQld2l92T89pBahLLBNTPr8_IcC5fNo3tfIaYDkB3iqMPbySsgp24KfNeAyH7KBjiVZ8NwVZIVYLiB2r6Uci1aKlF_ve0qPRJMPoBIdrMbLMe02jfakgL11VWmWgClq7TZ85BakBAc2WCNsvtSS2LVNYWEvXf6pDTnJ0TJk_wrveNUDZ_VFHLlltWPzbQBV0804BUwz86CU-Qw7I1XY-Of7BFrzKgI7wWu2MnpM0GOuD-Cob-QQBcEsHMsTGaOf6VkufpVuUNegePiGRtTK=w1897-h833-no?authuser=0"
       },
       {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "name":"Goings On",
+        "description":"MERN application to allow businesses to create an account and submit their daily/weekly specials. User can filter results to find local deals.",
+        "imgurl":"https://photos.google.com/photo/AF1QipNPst1wBgp5P2cz3xqg0e7hxbz7izn5VgPenStB"
       },
       {
         "name":"project3",
