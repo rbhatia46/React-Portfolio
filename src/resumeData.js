cd5f132731f920654e1d489aa416d1c462214d9f
Tyler Bolty <tsbolty@gmail.com>
2021-01-04 20:14:26 -0700

    added most of resume data

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,79 +1,79 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
+    "name": "Tyler Bolty",
+    "role": "Full Stack Developer",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": "I love creative problem solving and contributing to a team. Passion to learn and help others.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/tyler-bolty/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"http://github.com/tsbolty",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "url":"http://twitter.com/tsbolty",
           "className":"fa fa-twitter"
         }
       ],
     "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "address":"United States",
+    "website":"https://tsbolty.github.io/portfolio",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
+        "UniversityName":"The University of Denver",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"June",
         "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "Achievements":"Voted best team player by my peers"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"2U",
+        "specialization":"Full Stack Web Developer",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"",
+        "Achievements":"Top 1% in student satisfaction"
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"AT&T",
+        "specialization":"Sales Associate",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2020",
+        "Achievements":"Summit Award Winner 2017"
+      },
+      {
+        "CompanyName":"Samsung Electronics America",
+        "specialization":"Lead Experience Consultant",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2016",
+        "Achievements":"Oversaw 26 locations"
       }
     ],
-    "skillsDescription":"Your skills here",
+    "skillsDescription":"Top Skills",
     "skills":[
       {
-        "skillname":"HTML5"
+        "skillname":"React.js, HTML5, CSS3, JavaScript, JQuery, Bootstrap, Sass, Handlebars, UX"
       },
       {
-        "skillname":"CSS"
+        "skillname":"Node, Express.js, MongoDB, Mongoose, MySQL, Sequelize, Postgres, User Authentication, "
       },
       {
-        "skillname":"Reactjs"
+        "skillname":"Search-Engine-Optimization, Database Architecture, RESTful API's, Object-Oriented-Programming, Progressive Web Apps, Agile Development"
       }
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"Destination Gear",
+        "description":"MERN application to allow users to select their departure date and type of environment. A backpacking gear checklist will be generated and the user will have the ability to save notes for each location.",
+        "imgurl":"https://lh3.googleusercontent.com/ilitkk9VbLNVm6nbfnYaihJ1mcL3RIfXs0R96Ueg2IyPPgE8r0p4LEqxdnm3AqF8Osz7sQ2sQNn6QRWYb8G3QZx44WwlEH9lInY5ZV7hdr0Jl0o42ZDq18mMU4aHuHxhFfIuYn6aKldk97cVEWi1ZpOR2thWmhLgCrPUjQHlB-v2HyU7-m371EPkIudwTDyysiP0PdoksmXHZLD5puVXSZ7dcAtm3Hmf_xWCpesJEU902JwsG7QnCzdp8fE17YgWEklZdVFKrkeBcFbCbx-wznUeMiuBfsFBtDtDi-XDDlIbLF4FDGA7NpFguhUhiJ-Xa5ybbz_v0qu5P9ZjCYkLDPvekhGrjwPYYevpQLA9TkqaeDLzdTiIn9CbhxSFsCPx6uHODpS5HdToMsSdOFIZZnpJHSM9MPDQY_9gdFVCiVQld2l92T89pBahLLBNTPr8_IcC5fNo3tfIaYDkB3iqMPbySsgp24KfNeAyH7KBjiVZ8NwVZIVYLiB2r6Uci1aKlF_ve0qPRJMPoBIdrMbLMe02jfakgL11VWmWgClq7TZ85BakBAc2WCNsvtSS2LVNYWEvXf6pDTnJ0TJk_wrveNUDZ_VFHLlltWPzbQBV0804BUwz86CU-Qw7I1XY-Of7BFrzKgI7wWu2MnpM0GOuD-Cob-QQBcEsHMsTGaOf6VkufpVuUNegePiGRtTK=w1897-h833-no?authuser=0"
       },
       {
         "name":"project2",
