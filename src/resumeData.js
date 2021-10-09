18ed2e7669038139e208acc851b386441306b4a2
yungalyx <52767711+yungalyx@users.noreply.github.com>
2021-10-08 22:33:56 -0700

    hello

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,6 +1,6 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
+    "name": "Alex Lin",
     "role": "Frontend Developer and Data Scientist",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
