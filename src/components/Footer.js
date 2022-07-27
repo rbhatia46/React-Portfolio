7e0770a72b8457036efefb4caf3b12c419b32895
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2022-07-27 14:33:10 +0530

    initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class Footer extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <footer>
-      <div className="row">
-        <div className="twelve columns">
-          <ul className="social-links">
-            {
-              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
-                return(
-                  <li>
-                    <a href={item.url}>
-                    <i className={item.className} />
-                    </a>
-                  </li>
-                )
-              })
-            }
-          </ul>
-          
+        <div className="row" id="contacts">
+          <div className="twelve columns">
+            <ul className="social-links">
+              {resumeData.socialLinks &&
+                resumeData.socialLinks.map((item) => {
+                  return (
+                    <li>
+                      <a
+                        href={item.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className={item.className} />
+                      </a>
+                    </li>
+                  );
+                })}
+            </ul>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=rahulrajnkeht@gmail.com"
+            >
+              Email: rahulrajnkeht@gmail.com
+            </a>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open" />
+            </a>
+          </div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
-      </div>
-    </footer>
+      </footer>
     );
   }
 }
