64a8096d3ec9b788074ea71525789611c62e9ac9
shravanipagilla <nampally04@gmail.com>
2022-11-14 21:43:30 +0100

    Portfolio initiated

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,29 +1,29 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
+    "name": "Shravani Pagilla",
+    "role": "Fullstack Developer",
+    "linkedinId":"shravanipag",
+    "skypeid": "ShravaniPagilla",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/shravanipag/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/shravanipagilla",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "url":"http://twitter.com/shravanipagilla",
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
+    "aboutme":"I am currently student at HackYourFuture Copenhagen and pursuing Fullstack Developer intensive course, currently diving deeper into React coding. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Copenhagen",
     "website":"https://rbhatia46.github.io",
     "education":[
       {
