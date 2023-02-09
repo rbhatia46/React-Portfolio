dbb958a2bd0fc66f4a5769cce0fc5806c3aa07d0
Ryan <ryanlu294@gmail.com>
2023-02-08 16:15:29 -0800

    initialization for deploy test

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
-export default  class Resume extends Component {
+import React, { Component } from "react";
+export default class Resume extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="resume">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
 
-         <div className="row education">
-
-            <div className="three columns header-col">
-               <h1><span>Education</span></h1>
-            </div>
-
-            <div className="nine columns main-col">
-              {
-                resumeData.education && resumeData.education.map((item)=>{
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.UniversityName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
+          <div className="nine columns main-col">
+            {resumeData.education &&
+              resumeData.education.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>{item.UniversityName}</h3>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          {item.MonthOfPassing} {item.YearOfPassing}
+                        </em>
+                      </p>
+                      <p>{item.Achievements}</p>
                     </div>
-                  )
-                })
-              }
-            </div>
-         </div>
-        <div className="row work">
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        {/* <div className="row work">
             <div className="three columns header-col">
                <h1><span>Work</span></h1>
             </div>
@@ -56,44 +57,36 @@ export default  class Resume extends Component {
                   )
                 })
               }
-            </div> 
-         </div>
-
-
-         <div className="row skill">
-
-            <div className="three columns header-col">
-               <h1><span>Skills</span></h1>
             </div>
+         </div> */}
 
-            <div className="nine columns main-col">
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
 
-               <p>
-               {resumeData.skillsDescription}
-               </p>
+          <div className="nine columns main-col">
+            <p>{resumeData.skillsDescription}</p>
 
-   				<div className="bars">
-
-   				   <ul className="skills">
-                {
-                  resumeData.skills && resumeData.skills.map((item) => {
-                    return(
+            <div className="bars">
+              <ul className="skills">
+                {resumeData.skills &&
+                  resumeData.skills.map((item) => {
+                    return (
                       <li>
-                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
-                      </span><em>{item.skillname}</em>
+                        <span
+                          className={`bar-expand ${item.skillname.toLowerCase()}`}
+                        ></span>
+                        <em>{item.skillname}</em>
                       </li>
-                    )
-                  })
-                }
-
-   					</ul>
-
-   				</div>
-
-   			</div>
-
-         </div>
-
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
